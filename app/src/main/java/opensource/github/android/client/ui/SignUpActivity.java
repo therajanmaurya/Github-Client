75eae9207010237a803259a89bb4979d6461ff36
Rajan Maurya <rajanmaurya154@gmail.com>
2016-12-17 00:53:51 +0530

    User Sign Up UI Completed

--- app/src/main/java/opensource/github/android/client/ui/SignUpActivity.java
+++ app/src/main/java/opensource/github/android/client/ui/SignUpActivity.java
@@ -1,8 +1,13 @@
-package opensource.github.android.client.ui.user;
+package opensource.github.android.client.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.RelativeLayout;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import opensource.github.android.client.R;
 import opensource.github.android.client.ui.base.GitHubBaseActivity;
 
@@ -12,9 +17,19 @@ import opensource.github.android.client.ui.base.GitHubBaseActivity;
 
 public class SignUpActivity extends GitHubBaseActivity {
 
+    @BindView(R.id.rl_sign_up)
+    RelativeLayout rl_sign_in;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_user)
+    public void onClickUser() {
+        findViewById(R.id.btn_user).setVisibility(View.GONE);
+        rl_sign_in.setVisibility(View.VISIBLE);
     }
 }
---
+++ app/src/main/java/opensource/github/android/client/ui/repository/ReposFragment.java
@@ -0,0 +1,8 @@
+package opensource.github.android.client.ui.repository;
+
+/**
+ * Created by Rajan Maurya on 17/12/16.
+ */
+
+public class ReposFragment {
+}
